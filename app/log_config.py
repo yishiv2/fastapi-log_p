af8e0364ca3d71f8f4723939f09f0179d9b02975
yishiv2 <yishiv2@users.noreply.github.com>
2024-10-26 18:58:54 +0900

    ファイルlog追加

--- app/log_config.py
+++ app/log_config.py
@@ -4,7 +4,7 @@ import os
 
 def configure_logging():
     log_level = os.getenv("LOG_LEVEL", "INFO")  # 環境変数からログレベルを取得
-
+    
     dictConfig(
         {
             "version": 1,
@@ -15,6 +15,11 @@ def configure_logging():
                     "datefmt": "%Y-%m-%dT%H:%M:%S",
                     "format": "(%(name)s:%(lineno)d) - %(levelname)s - %(message)s",
                 },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(asctime)s - %(name)s:%(lineno)d - %(levelname)s - %(message)s",
+                },
             },
             "handlers": {
                 "default": {
@@ -22,17 +27,24 @@ def configure_logging():
                     "level": log_level,
                     "formatter": "console",
                 },
+                "file": {  # ファイルハンドラー
+                    "class": "logging.FileHandler",
+                    "level": log_level,
+                    "formatter": "file",
+                    "filename": "app/log/app.log",
+                    "encoding": "utf-8",
+                },
             },
             "loggers": {
-                "uvicorn": {"handlers": ["default"], "level": log_level},
+                "uvicorn": {"handlers": ["default", "file"], "level": log_level},
                 "src": {
-                    "handlers": ["default"],
+                    "handlers": ["default", "file"],
                     "level": log_level,
                     "propagate": False,
                 },
                 "sqlalchemy.engine": {  # SQLAlchemyエンジンのロガー
-                    "handlers": ["default"],
-                    "level": log_level,  # 環境変数から取得したログレベルを設定
+                    "handlers": ["default", "file"],
+                    "level": log_level,
                     "propagate": False,
                 },
             },
